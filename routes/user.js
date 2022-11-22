e00af1c062a406075090aa09f55665d5337ef421
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-11-22 21:47:58 +0100

    pull from dev branch

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 const userRouter = require("express").Router()
+const expressAsyncHandler = require("express-async-handler");
+
+
 const userController = require("../controllers/user")
 
+const {isAuth} = require('../middlewares/auth')
 
-userRouter.post("/signup", userController.userSignup);
+
+userRouter.post("/signup", expressAsyncHandler(userController.userSignup));
 userRouter.post("/login", userController.login);
+userRouter.post("/profile", isAuth, userController.handleUpdate);
 
 
 module.exports = userRouter; 
