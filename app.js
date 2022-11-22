ee9f90a0359aa702ebb50e3d06d918070b085353
victorAnumudu <anumuduchukwuebuka@gmail.com>
2022-11-22 17:49:25 +0100

    users routes for order done

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+const orderRoutes = require("./routes/order")
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", orderRoutes) // ROUTES FOR USER TO ACCESSING ORDER COLLECTION
 
 //Route Imports
 
