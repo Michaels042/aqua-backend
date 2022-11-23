7a892f949f9c51d1e382f202be287438fb92d509
ZUCO <113183892+ZUCOLINZ@users.noreply.github.com>
2022-11-23 10:37:49 +0100

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,42 +1,35 @@
-exports.userLogin = async function (req, res) {
-    const user = req.body;
-  
-    try {
-      const user =  await userSchema.validateAsync(req.body);
-      const existingUsher = await userModel.findOne({
-        email: user.email,
-      });
-  
-      if (existingUsher) {
-        if (bcrypt.compareSync(user.password, existingUsher.password)) {
-          const tokenPayload = {
-            id: existingUsher._id,
-            email: existingUsher.email,
-          };
-  
-          const accessToken = jwt.sign(
-            tokenPayload,
-            process.env.SECRET_KEY,
-            {
-              expiresIn: `3d`,
-            }
-          );
-          res.json({i
-            name: user.name,
-            msg: `user logged in successfully`,
-            accessToken: accessToken,
-          });
-        } else {
-          res.status(403).json({
-            success: false,
-            msg: "invalid password",
-          });
-        }
-      } else {
-        res.status(401).json({ success: false, msg: "user does not exist" });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500);
+exports.login = async (req, res) => {
+  let details;
+  let existingUser;
+  try {
+    details =  await loginSchema.validateAsync(req.body);
+  } catch (error) {
+   return res.status(500).json({ status:false, message:error.details[0].message});
+  }
+  // const { email, password } = req.body;
+  // let existingUser;
+
+  try {
+    existingUser = await userModel.findOne({ email: details.email });
+    if (!existingUser) {
+      return res.status(404).json({status: false, message: "User does not exist" });
     }
-  };
+    const isPasswordCorrect = bcrypt.compareSync(details.password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({status: false, message: "Email/password mismatched" });
+    };
+    
+    // Create token
+    const token = jwt.sign(
+      { id: existingUser._id, email: existingUser.email, role: existingUser.role },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2d",
+      }
+      );
+      
+    res.status(200).json({status: true, message: 'User logged in successfully', token})
+  } catch (err) {
+    return res.status(400).json({status: false, message: "Opps! something went wrong" });
+  }
+}
