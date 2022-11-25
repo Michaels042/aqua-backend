13c4c7ab344ce68a6a81a11c7d32e8f4ced2c4ad
rueby <ruthebubechi@gmail.com>
2022-11-25 12:40:43 +0100

    modified

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,19 +3,6 @@ const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-// export const generateToken = (user) => {
-//   return jwt.sign(
-//     {
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       isAdmin: user.isAdmin,
-//     },
-//     process.env.JWT_SECRET,
-//     { expiresIn: "10d" }
-//   );
-// };
-
 export const isAuth = (req, res, next) => {
   const { authorization } = req.headers;
 
