cc5660ecf3bcf190babfcd2b49891bba1a7a4358
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-11-25 21:18:44 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
-const userRouter = require("./routes/user")
+const cors = require("cors");
 
-const orderRoutes = require("./routes/order")
+const userRouter = require("./routes/user");
+
+const orderRoutes = require("./routes/order");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/auth", userRouter)
-app.use("/user", orderRoutes) // ROUTES FOR USER TO ACCESSING ORDER COLLECTION
+app.use(cors());
+app.use("/auth", userRouter);
+app.use("/user", orderRoutes); // ROUTES FOR USER TO ACCESSING ORDER COLLECTION
 
 //Route Imports
 
