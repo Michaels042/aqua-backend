205682580bfc4fcc147e02db1f7a6e8fad4a1b1a
zucolinz <collinsizuchukwu18@gmail.com>
2023-01-30 17:16:28 +0100

    first commit

--- app.js
+++ app.js
@@ -4,21 +4,16 @@ const cors = require("cors");
 
 const userRouter = require("./routes/user");
 
-const orderRoutes = require("./routes/order");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send({ status: true, message: "Welcome to aqua locator" });
+  res.send({ status: true, message: "Welcome to XYZ company" });
 }); // backend landing route
 
 app.use("/auth", userRouter);
-app.use("/user", orderRoutes); // ROUTES FOR USER TO ACCESSING ORDER COLLECTION
 
-app.use("/admin", orderRoutes); // ROUTES FOR ADMIN TO ACCESSING ORDER
-
-//Route Imports
 
 module.exports = app;
